1f1c4fbe60afcd03250dad08054c91f76d5ca249
Umichi <lite_dragon@hotmail.com>
2017-03-16 16:05:55 +1100

    Yuuki Database start

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Yuuki-chan's Fansub Database</title>
+	<meta name="description" content="Here you will find the new English releases by anime groups.">
+
+</head>
+
+<body>
+	<nav class="navBar">
+
+	</nav>
+
+	<div class="banner">
+
+	</div>
+
+	<main class="background">
+
+		<div class="tableTitle">
+
+			<div class="animeTable">
+
+				<table>
+
+				</table>
+
+			</div>
+
+		</div>
+
+	</main>
+
+	<footer class="footer">
+
+		<div class="copyright">
+		
+		</div>
+
+	</footer>
+
+</body>
+
+</html>
